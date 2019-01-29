ca125cd05298beedf7bed520fbd6b90303a65131
gsotov <gabriel.sotovalenzuela@gmail.com>
2019-01-29 09:29:04 -0300

    proyecto

---
+++ camara/CameraPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace camara
+{
+    public class CameraPage : ContentPage
+    {
+        public delegate void PhotoResultEventHandler(PhotoResultEventArgs result);
+
+        public event PhotoResultEventHandler OnPhotoResult;
+
+        public void SetPhotoResult(byte[] image, int width = -1, int height = -1)
+        {
+            OnPhotoResult?.Invoke(new PhotoResultEventArgs(image, width, height));
+        }
+
+        public void Cancel()
+        {
+            OnPhotoResult?.Invoke(new PhotoResultEventArgs());
+        }
+    }
+
+    public class PhotoResultEventArgs : EventArgs
+    {
+
+        public PhotoResultEventArgs()
+        {
+            Success = false;
+        }
+
+        public PhotoResultEventArgs(byte[] image, int width, int height)
+        {
+            Success = true;
+            Image = image;
+            Width = width;
+            Height = height;
+        }
+
+        public byte[] Image { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public bool Success { get; private set; }
+    }
+}
---
+++ camara/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace camara
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TakePhotoButton.Clicked += TakePhotoButton_Clicked;;
+        }
+
+        async void TakePhotoButton_Clicked(object sender, System.EventArgs e)
+        {
+            var cameraPage = new CameraPage();
+            cameraPage.OnPhotoResult += CameraPage_OnPhotoResult;
+            await Navigation.PushModalAsync(cameraPage);
+        }
+
+
+
+        async void CameraPage_OnPhotoResult(PhotoResultEventArgs result)
+        {
+            await Navigation.PopModalAsync();
+            if (!result.Success)
+                return;
+
+            Photo.Source = ImageSource.FromStream(() => new MemoryStream(result.Image));
+        }
+
+    }
+}
